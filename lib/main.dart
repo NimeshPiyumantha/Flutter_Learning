7dcaa5a5194f9d06f1f8c2c6f35bad9b6088ce92
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-06-22 18:40:46 +0530

    Update Content

--- lib/main.dart
+++ lib/main.dart
@@ -56,7 +56,6 @@ class _MyHomePageState extends State<MyHomePage> {
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
-              
             ),
           ],
         ),
