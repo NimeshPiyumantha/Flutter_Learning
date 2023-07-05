5304598355fece6b4461f9b78511d4804ee39128
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-07-05 23:47:26 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('App Title'),
+        ),
+      ),
     );
   }
 }
