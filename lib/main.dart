f9057eab0016d861dadad840d2a489b76040406d
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-07-08 00:13:05 +0530

    Refacor project

--- lib/main.dart
+++ lib/main.dart
@@ -12,9 +12,9 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: Text('App Title'),
+          title: const Text('App Title'),
         ),
-        body: Text('Body'),
+        body: const Text('Body'),
         bottomNavigationBar: BottomNavigationBar(
           items: [
             BottomNavigationBarItem(icon: icon)
