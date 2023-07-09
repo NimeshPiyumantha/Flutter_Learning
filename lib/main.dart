a37c6c882d012af65563709e0db0e32d5a7bad15
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-07-09 10:47:19 +0530

    Add new Navigator 2

--- lib/main.dart
+++ lib/main.dart
@@ -4,56 +4,99 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Centered Button',
-        home: FirstPage());
+        debugShowCheckedModeBanner: false, home: MyAppExt());
   }
 }
 
-class FirstPage extends StatelessWidget {
-  const FirstPage({super.key});
+class MyAppExt extends StatefulWidget {
+  const MyAppExt({super.key});
+
+  @override
+  _MyAppExtState createState() => _MyAppExtState();
+}
+
+class _MyAppExtState extends State<MyAppExt> {
+  String buttonName = 'Click';
+  int currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Title'),
+        title: const Text('App Title'),
       ),
       body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            int index;
-            String name;
-            // Button pressed action
-            Navigator.of(context).push(
-              MaterialPageRoute(
-                builder: (BuildContext context) {
-                  return const SecondPage();
-                },
-              ),
-            );
-          },
-          child: const Text('Press'),
-        ),
+        child: currentIndex == 0
+            ? Container(
+                width: double.infinity,
+                height: double.infinity,
+                color: Colors.red,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        onPrimary: Colors.black,
+                        primary: Colors.orange,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          buttonName = 'Clicked';
+                        });
+                      },
+                      child: Text(buttonName),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (BuildContext context) => const NextPage(),
+                          ),
+                        );
+                      },
+                      child: const Text('Next Page'),
+                    ),
+                  ],
+                ),
+              )
+            : Image.asset('assets/images/bg.png'),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(label: 'Home', icon: Icon(Icons.home)),
+          BottomNavigationBarItem(
+              label: 'Settings', icon: Icon(Icons.settings)),
+        ],
+        currentIndex: currentIndex,
+        onTap: (int index) {
+          setState(() {
+            currentIndex = index;
+          });
+        },
       ),
     );
   }
 }
 
-class SecondPage extends StatelessWidget {
-  const SecondPage({Key? key}) : super(key: key);
+class NextPage extends StatelessWidget {
+  const NextPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // automaticallyImplyLeading: false,
         title: const Text('Second Page'), // Custom title for the AppBar
       ),
       body: const Center(
