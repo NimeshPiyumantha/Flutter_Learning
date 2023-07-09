5ca80098228744d6198a8647ed6d8ae2c0b663c8
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-07-09 10:18:23 +0530

    Refacor project

--- lib/main.dart
+++ lib/main.dart
@@ -4,74 +4,59 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  String buttonName = 'Click';
-  int currentIndex = 0;
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('App Title'),
-        ),
-        body: Center(
-          child: currentIndex == 0
-              ? Container(
-                  width: double.infinity,
-                  height: double.infinity,
-                  color: Colors.red,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          onPrimary: Colors.black,
-                          primary: Colors.orange,
-                        ),
-                        onPressed: () {
-                          setState(() {
-                            buttonName = 'Clicked';
-                          });
-                        },
-                        child: Text(buttonName),
-                      ),
-                      ElevatedButton(
-                        onPressed: () {
-                          setState(() {
-                            buttonName = 'Clicked';
-                          });
-                        },
-                        child: Text(buttonName),
-                      ),
-                    ],
-                  ),
-                )
-              : Image.asset(
-                  'assets/images/bg.png'),
-        ),
-        bottomNavigationBar: BottomNavigationBar(
-          items: const [
-            BottomNavigationBarItem(label: 'Home', icon: Icon(Icons.home)),
-            BottomNavigationBarItem(
-                label: 'Settings', icon: Icon(Icons.settings)),
-          ],
-          currentIndex: currentIndex,
-          onTap: (int index) {
-            setState(() {
-              currentIndex = index;
-            });
+    return const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Centered Button',
+        home: FirstPage());
+  }
+}
+
+class FirstPage extends StatelessWidget {
+  const FirstPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Title'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            int index;
+            String name;
+            // Button pressed action
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (BuildContext context) {
+                  return const SecondPage();
+                },
+              ),
+            );
           },
+          child: const Text('Press'),
         ),
+      ),
+    );
+  }
+}
+
+class SecondPage extends StatelessWidget {
+  const SecondPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Second Page'), // Custom title for the AppBar
+      ),
+      body: const Center(
+        child: Text('This is the second page'),
       ),
     );
   }
