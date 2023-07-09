864401c31ba1f99240df5b8bf0c1a3642fe43957
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-07-09 09:33:14 +0530

    Add image

--- lib/main.dart
+++ lib/main.dart
@@ -24,37 +24,41 @@ class _MyAppState extends State<MyApp> {
           title: const Text('App Title'),
         ),
         body: Center(
-            child: Container(
-          width: double.infinity,
-          height: double.infinity,
-          color: Colors.red,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  onPrimary: Colors.black,
-                  primary: Colors.orange,
-                ),
-                onPressed: () {
-                  setState(() {
-                    buttonName = 'Clicked';
-                  });
-                },
-                child: Text(buttonName),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  setState(() {
-                    buttonName = 'Clicked';
-                  });
-                },
-                child: Text(buttonName),
-              ),
-            ],
-          ),
-        )),
+          child: currentIndex == 0
+              ? Container(
+                  width: double.infinity,
+                  height: double.infinity,
+                  color: Colors.red,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          onPrimary: Colors.black,
+                          primary: Colors.orange,
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            buttonName = 'Clicked';
+                          });
+                        },
+                        child: Text(buttonName),
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            buttonName = 'Clicked';
+                          });
+                        },
+                        child: Text(buttonName),
+                      ),
+                    ],
+                  ),
+                )
+              : Image.network(
+                  'https://i.pinimg.com/originals/72/47/f3/7247f32dedcabe39c7c9c8d06a483083.png'),
+        ),
         bottomNavigationBar: BottomNavigationBar(
           items: const [
             BottomNavigationBarItem(label: 'Home', icon: Icon(Icons.home)),
